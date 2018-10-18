43192e6814c69e0a267df497b3a12ab276b3fe0e
Donald.Z <chinasdzyz@163.com>
2018-10-18 19:48:25 +0800

    Add define file. Ref #1

---
+++ delimiter.h
@@ -0,0 +1,6 @@
+#define Delimiter_SEMICOLON 0
+#define Delimiter_LEFT_BRACE 1
+#define Delimiter_RIGHT_BRACE 2
+#define Delimiter_COMMA 3
+#define Delimiter_LINE_BREAK 4
+#define Delimiter_CARRIAGE_RETURN 5
---
+++ typename.h
@@ -0,0 +1,6 @@
+#define Type_CHAR 0
+#define Type_DOUBLE 1
+#define Type_FLOAT 2
+#define Type_INT 3
+#define Type_SHORT 4
+#define Type_VOID 5
